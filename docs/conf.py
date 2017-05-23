23ada9e8797679f44029bb89daee8bfc60593680
Matt Bellis <matthew.bellis@gmail.com>
2017-05-23 12:02:17 -0400

    Edited the docs a bit.

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,10 @@
 # add these directories to sys.path here. If the directory is relative to the
 import sys
 import os
-sys.path.insert(0, os.path.abspath(os.pardir))
+#sys.path.insert(0, os.path.abspath(os.pardir))
+#sys.path.append('/home/bellis/hmis')
+#sys.path.insert(0, os.path.abspath(os.pardir))
+sys.path.append(os.path.abspath(os.pardir))
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 # import os
