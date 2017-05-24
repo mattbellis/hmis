9a5cd96a1e698cf84baa779ab543c41acc777462
saramahar <se30maha@siena.edu>
2017-05-24 10:34:04 -0400

    Separated the functions to visualization, parsing, and selection

--- hmis/parsing.py
+++ hmis/parsing.py
@@ -98,27 +98,7 @@ def read_in_data(directory='~/hmis_data/',filenames=None,verbose=False):
 
 
 
-################################################################################
-# Get age from birthdate.
-################################################################################
-def calc_age(birthdate):
-    """ This function converts the birthdate to a datetime object and subtracts today's date. This calculates the age of an individual.
-    
-    Args:
-        birthdate (string): The birthday of the individual.
-            
-    Returns:
-        age (string): The age of the individual.
-    
-    
-    """
-    
-    birth_year,birth_month,birth_day = birthdate.split('-')
-    born = dt.datetime(int(birth_year),int(birth_month),int(birth_day))
-    today = datetime.now()
-    age= today.year-born.year - ((today.month,today.day) < (born.month, born.day))
 
-    return age
 
 
 ################################################################################
@@ -272,38 +252,7 @@ def save_file(inds,filename):
     outfile.close()
 
 
-################################################################################
-# Gets IDs within a certain age range.
-################################################################################
-def get_subset_with_age_range(filename,lo=0, hi=1e9, matching_key='Personal ID'):
-    """ This function finds the individuals within the age range and returns the dictionaries of those individuals. 
-    
-    Args:
-        filename (string): The name of the file that holds all of the dictionaries.
-        lo (int): The lower bound of the targeted age range.
-        Defaults to: 0
-        hi (int): The upper bound of the targeted age range.
-        Defaults to: 1e9
-        matching_key (string): The value that determines the cross referencing between the files. 
-        Defaults to: 'Personal ID'
-    
-    Returns: 
-        people (list): The list of personal IDs that are within the range inputted. 
-        
-    """
-    
-    # Gets the personal IDs within the age range specified. 
-    people=[]
-    for num,ind in enumerate(filename):
-        if ind['Age']>=lo and ind['Age']<=hi:
-            people.append(ind['Personal ID'])
-    people=np.unique(people)
-    people.sort()
-    print((len(people)))
-    
-    ppl = get_subset_from_dictionary(people,filename)
-    
-    return ppl
+
 
 
 ################################################################################
@@ -342,61 +291,7 @@ def get_subset_from_dictionary(names,full_dictionary,matching_key='Personal ID')
 
     
     
- 
-    
-    
-    
-    
-    
-    
-################################################################################
-# The following functions are to read in the data for the specified personal IDs.
-################################################################################
 
-
-################################################################################
-# Get all of the personal IDs within an age range.
-################################################################################
-def get_dictionaries_with_age_range(lo=0,hi=1e9):
-    """ This function finds the individuals within the age range and returns the personal IDs of those individuals. 
-    
-    Args:
-        filename (string): The name of the file that holds all of the dictionaries.
-        lo (int): The lower bound of the targeted age range.
-        Defaults to: 0
-        hi (int): The upper bound of the targeted age range.
-        Defaults to: 1e9
-    
-    Returns: 
-        people (list): The list of personal IDs that are within the range inputted. 
-        
-    """
-    
-    filename = read_in_data(filenames=['Client.csv'])
-    filename = filename[0]
-    namesCL = filename['PersonalID']
-     
-    client_dob = filename['DOB']
-    people=[]
-    for num,x in enumerate(client_dob):
-
-        month,day,year = x.split('-')
-        birthdate = dt.datetime(int(month),int(day),int(year))
-
-        age = dt.datetime.now() - birthdate
-        age_in_years = np.floor(np.abs(age.days/365.))
-
-        if age_in_years>=lo and age_in_years<=hi:
-            people.append(namesCL[num])
-    return people
-    
-    
-    
-    
-    
-    
-    
-    
 
 
 
---
+++ hmis/selection.py
@@ -0,0 +1,69 @@
+
+
+
+
+
+
+
+
+################################################################################
+# Get age from birthdate.
+################################################################################
+def calc_age(birthdate):
+    """ This function converts the birthdate to a datetime object and subtracts today's date. This calculates the age of an individual.
+    
+    Args:
+        birthdate (string): The birthday of the individual.
+            
+    Returns:
+        age (string): The age of the individual.
+    
+    
+    """
+    
+    birth_year,birth_month,birth_day = birthdate.split('-')
+    born = dt.datetime(int(birth_year),int(birth_month),int(birth_day))
+    today = datetime.now()
+    age= today.year-born.year - ((today.month,today.day) < (born.month, born.day))
+
+    return age
+
+
+
+
+################################################################################
+# Gets IDs within a certain age range.
+################################################################################
+def get_subset_with_age_range(filename,lo=0, hi=1e9, matching_key='Personal ID'):
+    """ This function finds the individuals within the age range and returns the dictionaries of those individuals. 
+    
+    Args:
+        filename (string): The name of the file that holds all of the dictionaries.
+        lo (int): The lower bound of the targeted age range.
+        Defaults to: 0
+        hi (int): The upper bound of the targeted age range.
+        Defaults to: 1e9
+        matching_key (string): The value that determines the cross referencing between the files. 
+        Defaults to: 'Personal ID'
+    
+    Returns: 
+        people (list): The list of personal IDs that are within the range inputted. 
+        
+    """
+    
+    # Gets the personal IDs within the age range specified. 
+    people=[]
+    for num,ind in enumerate(filename):
+        if ind['Age']>=lo and ind['Age']<=hi:
+            people.append(ind['Personal ID'])
+    people=np.unique(people)
+    people.sort()
+    print((len(people)))
+    
+    ppl = get_subset_from_dictionary(people,filename)
+    
+    return ppl
+
+
+
+
